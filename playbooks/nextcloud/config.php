3b2e7082a33c342acd6cc2751d59bcb4eab65aa5
Lilian Mallardeau <lilianmallardeau@gmail.com>
2023-05-06 04:29:30 +0200

    First stuff I do with Ansible yey!

---
+++ playbooks/nextcloud/config.php
@@ -0,0 +1,8 @@
+<?php
+$CONFIG = array (
+    'datadirectory' => '/srv/nextcloud/',
+    'default_phone_region' => 'FR',
+    'memcache.local' => '\OC\Memcache\APCu',
+    'overwrite.cli.url' => 'https://nextcloud.local/',
+    'htaccess.RewriteBase' => '/',
+);
