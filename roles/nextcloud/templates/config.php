0ace5e77a3eac8f079d752cdc9972d79a8b3ee7b
Lilian Mallardeau <lilianmallardeau@gmail.com>
2023-06-09 00:21:43 +0200

    Moved roles into their own repo

--- roles/nextcloud/templates/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$CONFIG = array (
-    'datadirectory' => '{{ nextcloud_data_directory }}/',
-    'default_phone_region' => '{{ nextcloud_default_phone_region }}',
-    'memcache.local' => '\OC\Memcache\APCu',
-    'overwrite.cli.url' => 'https://{{ nextcloud_domain_name }}/',
-    'htaccess.RewriteBase' => '/',
-);
